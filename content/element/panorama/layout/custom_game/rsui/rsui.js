c770f085a0ed6794776dad87fbd30b4262e187dd
vasiliev-v <143706208+vasiliev-v@users.noreply.github.com>
2026-03-10 20:47:21 +0100

    Reorganize Panorama custom_game files into panel folders

---
+++ content/element/panorama/layout/custom_game/rsui/rsui.js
@@ -0,0 +1,165 @@
+var list = [];
+var skiponetick = false
+function AddRSUI( data )
+{
+    $.Msg(data);
+    list.push(data);
+}
+
+function IntTick()
+{
+    if (skiponetick == false)
+    {
+        if ($("#PanelForPart") != null)
+        {
+            //$("#immortalpart").visible = false;
+            //$("#immortalpart").DeleteAsync(0);
+            $("#PanelForPart").RemoveAndDeleteChildren();
+            //$("#immortalpart").visible = true;
+        }
+        if (list[0] != null)
+        {
+            $("#Line1").visible = true;
+            if ($("#item1") != null)
+            {
+                $("#item1").RemoveAndDeleteChildren();
+            }
+            if ($("#Line1").BHasClass("OffPanelClass"))
+                $("#Line1").RemoveClass("OffPanelClass");
+            if ($("#Line1").BHasClass("OffPanelClassImm"))
+                $("#Line1").RemoveClass("OffPanelClassImm");
+
+            switch(list[0]["rsid"].charAt(0)) {
+                case '1':
+                rares = "shards";
+                myclass = "common";
+                break
+            
+                case '2':
+                rares = "stones";
+                myclass = "rare";
+                break
+            
+                case '3':
+                rares = "crystals";
+                myclass = "mythical";
+                break
+            
+                case '4':
+                rares = "clusters";
+                myclass = "immortal";
+                skiponetick = true;
+                break
+            }
+            if (myclass == "immortal")
+            {
+                $("#PanelForPart").RemoveAndDeleteChildren();
+                var immortalPart = $.CreatePanel("DOTAScenePanel", $("#PanelForPart"), "immortalpart");
+                immortalPart.hittest = false;
+                immortalPart.particleonly = "true";
+                immortalPart.style.marginTop = "-135px";
+                immortalPart.style.marginLeft = "-52px";
+                immortalPart.style.height = "300px";
+                immortalPart.style.width = "300px";
+                immortalPart.map = "cameras";
+                immortalPart.camera = "partcamera4";
+            }
+            if (myclass == "immortal")
+            {
+                $("#Line1").AddClass("OffPanelClassImm");
+            }
+            else
+            {
+                $("#Line1").AddClass("OffPanelClass");
+            }
+            switch(list[0]["rsid"].charAt(1)) {
+                case '0':
+                elem = "ice";
+                break
+            
+                case '1':
+                elem = "fire";
+                break
+            
+                case '2':
+                elem = "water";
+                break
+            
+                case '3':
+                elem = "energy";
+                break
+            
+                case '4':
+                elem = "earth";
+                break
+            
+                case '5':
+                elem = "life";
+                break
+            
+                case '6':
+                elem = "void";
+                break
+            
+                case '7':
+                elem = "air";
+                break
+            
+                case '8':
+                elem = "light";
+                break
+            
+                case '9':
+                elem = "shadow";
+                break
+            }
+            $("#hero1").heroname = list[0]["hero"];
+            var item1 = $.CreatePanel("Panel", $("#Line1"), "item1");
+            item1.hittest = false;
+            item1.style.marginTop = "4px";
+            item1.style.marginLeft = "82px";
+            item1.SetPanelEvent("onmouseover", (function(rsid)
+            {
+                return function()
+                {
+                    $.DispatchEvent("UIShowCustomLayoutParametersTooltip", "RSTooltip", "file://{resources}/layout/custom_game/rs_tooltips/rs_tooltips.xml", "num=" + rsid);
+                };
+            })(list[0]["rsid"]));
+            item1.SetPanelEvent("onmouseout", function()
+            {
+                $.DispatchEvent("UIHideCustomLayoutTooltip", "RSTooltip");
+            });
+
+            var itemImage = $.CreatePanel("Image", item1, "");
+            itemImage.SetImage("file://{images}/custom_game/relic_items/"+rares+"/"+elem+"/"+list[0]["rsid"].charAt(2)+".png");
+            itemImage.style.height = "36px";
+            itemImage.style.width = "48px";
+
+            var classPanel = $.CreatePanel("Panel", item1, "");
+            classPanel.hittest = false;
+            classPanel.AddClass(myclass);
+            classPanel.style.height = "36px";
+            classPanel.style.width = "3px";
+            classPanel.style.marginLeft = "48px";
+            list.shift();
+            //$("#immortalpart").visible = true;
+            //$.Msg(list);
+        }
+        else
+        {
+            $("#Line1").visible = false;
+        }
+    }
+    else
+    {
+        skiponetick = false;
+    }
+    $.Schedule(4,IntTick);
+}
+
+(function()
+{
+    $("#Line1").visible = false;
+    GameEvents.SubscribeProtected( "AddRSUI", AddRSUI);
+    IntTick();
+})();
---
+++ content/element/panorama/layout/custom_game/scripts/setup.js
@@ -0,0 +1,121 @@
+	function InitCustomUI()
+{
+	//GameUI.SetDefaultUIEnabled( DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_TIMEOFDAY, false );      //Time of day (clock).
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_HEROES, false);     //Heroes and team score at the top of the HUD.
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_FLYOUT_SCOREBOARD, false);      //Lefthand flyout scoreboard.
+    //GameUI.SetDefaultUIEnabled( DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ACTION_PANEL, false );     //Hero actions UI.
+    GameUI.SetDefaultUIEnabled( DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ACTION_MINIMAP, false );     //Minimap.
+    //GameUI.SetDefaultUIEnabled( DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_PANEL, false );      //Entire Inventory UI
+    //GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_SHOP, false);     //Shop portion of the Inventory.
+    //GameUI.SetDefaultUIEnabled( DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_ITEMS, false );      //Player items.
+    //GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_QUICKBUY, false);     //Quickbuy.
+    //GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_COURIER, false);      //Courier controls.
+    //GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_PROTECT, false);      //Glyph.
+    //GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_GOLD, false);     //Gold display.
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_SHOP_SUGGESTEDITEMS, false);      //Suggested items shop panel.
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_HERO_SELECTION_TEAMS, false);     //Hero selection Radiant and Dire player lists.
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_HERO_SELECTION_GAME_NAME, false);     //Hero selection game mode name display.
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_HERO_SELECTION_CLOCK, false);     //Hero selection clock.
+    //GameUI.SetDefaultUIEnabled( DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_MENU_BUTTONS, false );     //Top-left menu buttons in the HUD.
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ENDGAME, false);      //Endgame scoreboard.
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_BAR_BACKGROUND, false);     //Top-left menu buttons in the HUD.
+    GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_FIGHT_RECAP, false);     //
+
+	GameUI.CustomUIConfig().multiteam_top_scoreboard =
+		{
+			reorder_team_scores: true,
+			LeftInjectXMLFile: "file://{resources}/layout/custom_game/overthrow_scoreboard_left/overthrow_scoreboard_left.xml",
+			TeamOverlayXMLFile: "file://{resources}/layout/custom_game/overthrow_scoreboard_team_overlay/overthrow_scoreboard_team_overlay.xml"
+		};
+
+		GameUI.CustomUIConfig().team_logo_xml = "file://{resources}/layout/custom_game/overthrow_team_icon/overthrow_team_icon.xml";
+		GameUI.CustomUIConfig().team_logo_large_xml = "file://{resources}/layout/custom_game/overthrow_team_icon_large/overthrow_team_icon_large.xml";
+
+		GameUI.SetDefaultUIEnabled( DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ENDGAME, false );
+
+		GameUI.CustomUIConfig().team_colors = {}
+		GameUI.CustomUIConfig().team_colors[DOTATeam_t.DOTA_TEAM_GOODGUYS] = "#00ff00;"; // { 61, 210, 150 }	--		Teal
+		GameUI.CustomUIConfig().team_colors[DOTATeam_t.DOTA_TEAM_BADGUYS ] = "#ff0000;"; // { 243, 201, 9 }		--		Yellow
+
+		GameUI.CustomUIConfig().team_icons = {}
+		GameUI.CustomUIConfig().team_icons[DOTATeam_t.DOTA_TEAM_GOODGUYS] = "s2r://panorama/images/custom_game/team_icons/team_icon_tiger_01.png";
+		GameUI.CustomUIConfig().team_icons[DOTATeam_t.DOTA_TEAM_BADGUYS ] = "s2r://panorama/images/custom_game/team_icons/team_icon_monkey_01.png";
+
+		 var tooltipManager = $.GetContextPanel().GetParent().GetParent().FindChildTraverse("Tooltips");
+    	tooltipManager.AddClass("CustomTooltipStyle");
+
+    	var newUI = $.GetContextPanel().GetParent().GetParent().FindChildTraverse("HUDElements");
+    	var centerBlock = newUI.FindChildTraverse("center_block");
+
+		let minimap_container = FindDotaHudElement("minimap_container")  
+		 
+		let TormentorTimerContainer = FindDotaHudElement("TormentorTimerContainer")
+    	if (TormentorTimerContainer)
+   		{
+        	TormentorTimerContainer.style.visibility = "collapse"
+    	}
+		
+		//FindDotaHudElement("GameInfoButton").style.visibility = "collapse";
+		FindDotaHudElement("PreMinimapContainer").style.visibility = "collapse";
+		FindDotaHudElement("FriendsAndFoes").style.visibility = "collapse";
+		FindDotaHudElement("BattlePassContainer").style.visibility = "collapse";
+		FindDotaHudElement("PlusChallengeContainer").style.visibility = "collapse";
+		FindDotaHudElement("StrategyTabTopRow").style.visibility = "collapse";
+		//FindDotaHudElement("courier").style.visibility = "collapse";
+		//FindDotaHudElement("CommonItems").style.visibility = "collapse";
+		//FindDotaHudElement("QuickBuySlot8").style.visibility = "collapse";
+		FindDotaHudElement("GlyphScanContainer").style.visibility = "collapse";
+		newUI.FindChildTraverse("inventory_neutral_craft_holder").style.visibility = "collapse";  
+
+		var shopManager = $.GetContextPanel().GetParent().GetParent().FindChildTraverse("shop");
+		shopManager.FindChildTraverse("GridNeutralsTab").style.visibility = "collapse";
+		shopManager.FindChildTraverse("RequestSuggestion").style.visibility = "collapse"; 
+		shopManager.FindChildTraverse("PopularItems").style.visibility = "collapse"; 
+		shopManager.FindChildTraverse("BuybackProtection").style.visibility = "collapse"; 
+
+		centerBlock.FindChildTraverse("ContentsContainer").GetParent().style.visibility = "collapse";
+		centerBlock.FindChildTraverse("StatBranch").style.visibility = "collapse";
+		centerBlock.FindChildTraverse("StatBranch").SetPanelEvent("onmouseover", function () {
+		});
+		centerBlock.FindChildTraverse("StatBranch").SetPanelEvent("onactivate", function () {
+		});
+
+		// Remove xp circle
+	// centerBlock.FindChildTraverse("xp").style.visibility = "collapse";
+	// centerBlock.FindChildTraverse("stragiint").style.visibility = "collapse";
+		//Fuck that levelup button
+		centerBlock.FindChildTraverse("level_stats_frame").style.visibility = "collapse";
+		// Hide tp slot
+		centerBlock.FindChildTraverse("inventory_tpscroll_container").style.visibility = "collapse";
+
+		centerBlock.FindChildTraverse("inventory_tpscroll_HotkeyContainer").style.visibility = "collapse";
+
+
+		}
+		
+
+
+InitCustomUI();
+
+
+function FindDotaHudElement(sId)
+{
+	return GetDotaHud().FindChildTraverse(sId);
+}
+
+function GetDotaHud()
+{
+	let hPanel = $.GetContextPanel();
+
+	while ( hPanel && hPanel.id !== 'Hud')
+	{
+        hPanel = hPanel.GetParent();
+	}
+
+	if (!hPanel)
+	{
+        throw new Error('Could not find Hud root from panel with id: ' + $.GetContextPanel().id);
+	}
+
+	return hPanel;
+}
