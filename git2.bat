ce6aeecf8be5270cecd16f4110715d9e0f267fe6
yancheex2 <yancheex2@Inbox.ru>
2026-05-31 00:53:47 +0500

    цйу
    цуй

--- git2.bat
+++ git2.bat
@@ -1,3 +1,3 @@
-mklink /J "D:\dota2\element2\game\element\" "E:\SteamLibrary\steamapps\common\dota 2 beta\game\dota_addons\element\"
+mklink /J "C:\vlad\element2\game\element\" "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota_addons\element\"
 
-mklink /J "D:\dota2\element2\content\element\" "E:\SteamLibrary\steamapps\common\dota 2 beta\content\dota_addons\element\"
+mklink /J "C:\vlad\element2\content\element\" "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\content\dota_addons\element\"
